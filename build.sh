5af29097685845539891e7f6e2e403489321f999
unsafe9 <realkorokke@outlook.com>
2024-11-19 19:05:13 +0900

    Fix build script

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e
 
 dotnet publish -c Release -r osx-arm64
-cp bin/Release/osx-arm64/publish/protoc-gen-turbolink ../Cricket/Plugins/TurboLink/Tools/protoc-gen-turbolink
-chmod +x ../Cricket/Plugins/TurboLink/Tools/protoc-gen-turbolink
+cp bin/Release/osx-arm64/publish/protoc-gen-turbolink ../cricket-server/tools/protoc/protoc-gen-turbolink
+chmod +x ../cricket-server/tools/protoc/protoc-gen-turbolink
 
 dotnet publish -c Release -r win-x64
-cp bin/Release/win-x64/publish/protoc-gen-turbolink.exe ../Cricket/Plugins/TurboLink/Tools/protoc-gen-turbolink.exe
+cp bin/Release/win-x64/publish/protoc-gen-turbolink.exe ../cricket-server/tools/protoc/protoc-gen-turbolink.exe
